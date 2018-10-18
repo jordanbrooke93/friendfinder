b963033e35ce118deecb4864a822d0128d609aaf
jordanbrooke93 <jordantaylor93@me.com>
2018-10-18 17:13:24 -0400

    added some stuff

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,23 @@
+// array that holds the info for those that have taken the survey
+// started it with made up data
+
+var friends = [
+    {
+        name: "Joey Tribbiani",
+        picture: "images/joeytribbiani.jpg",
+        scores: [
+            "3",
+            "5",
+            "5",
+            "2",
+            "3",
+            "1",
+            "3",
+            "4",
+            "5",
+            "4"
+        ]
+    }
+]
+
+module.exports = friends
